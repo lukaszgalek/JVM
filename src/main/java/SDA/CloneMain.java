a6f93c09e7668884de7dc79b58ca024e7bb380d3
Lukasz Galek <lukasz.galek@o2.pl>
2019-02-24 13:08:50 +0100

    course klonowanie wspolnego obiektu- obiekt w obiekcie

--- src/main/java/SDA/CloneMain.java
+++ src/main/java/SDA/CloneMain.java
@@ -5,6 +5,10 @@ public class CloneMain {
         Employee emp = new Employee("Abhi");
         try {
             Employee emp2 = (Employee) emp.clone();
+            // (Employee) bo trzeba rzutować z object ( clone nalezy do object) na obiekt Employee
+            //Object object = emp;
+            // rzutowanie objekt innego typu  - ale pochodnych powiazanych ze sobą dziedziczących ( ze obiekt ogólny ma sie zachowywać jak obiekt konkretny ) narzucienie konkretnej roli
+            // parsoanie tłumaczenie jednego obiektu na drugi  - obiektów nie powiązanych ze soba - tłumacz
             System.out.println(emp2.getName());
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
